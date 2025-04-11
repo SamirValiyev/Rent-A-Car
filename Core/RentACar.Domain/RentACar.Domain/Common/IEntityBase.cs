876409f79cabeca25e55e53c2c2bb93fa9a8e7bc
SamirValiyev <vliyevsamir5@gmail.com>
2025-04-11 15:58:09 +0400

    Common added

---
+++ Core/RentACar.Domain/RentACar.Domain/Common/IEntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Domain.Common
+{
+    public interface IEntityBase
+    {
+         int Id { get; set; }
+         DateTime CreatedDate { get; set; }
+         DateTime UpdatedDate { get; set; }
+         bool IsDeleted { get; set; }
+    }
+}
